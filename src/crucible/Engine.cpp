56e367b7ffb4e3088743e5dfc7a06d861ef30fc4
jshelton <joashel@gmail.com>
2023-10-19 16:25:12 -0600

    started embed .net instead of mono

--- src/crucible/Engine.cpp
+++ src/crucible/Engine.cpp
@@ -1,16 +1,14 @@
 #include "Engine.h"
 #include <slag/SlagLib.h>
 #include <SDL.h>
-#include <mono/jit/jit.h>
-#include <mono/metadata/assembly.h>
-#include <boost/filesystem.hpp>
-#include <boost/dll.hpp>
+#include <hostfxr.h>
+#include <nethost.h>
+#include <cassert>
 
 using namespace slag;
 namespace crucible
 {
-    MonoDomain* rootDomain = nullptr;
-    MonoDomain* appDomain = nullptr;
+
     void Engine::initialize()
     {
         SDL_Init(SDL_INIT_EVERYTHING);
@@ -18,31 +16,10 @@ namespace crucible
         details.backend = VULKAN;
         SlagLib::initialize(details);
 
-        auto path = boost::dll::program_location().parent_path().string();
-        std::replace( path.begin(), path.end(), '/', '\\');
-
-        //mono
-        mono_set_dirs(path.c_str(), path.c_str());
-        // Initialise mono runtime
-        rootDomain = mono_jit_init("Crucible");
-
-
-        // Create your app domain, this is what stores your loaded assemblies in memory.
-        //  Second parameter is optional configuration file path
-        char name[]="CrucibleRuntime";
-        appDomain = mono_domain_create_appdomain(&name[0], nullptr);
-        assert(appDomain && "Unable to start mono runtime");
-
-        // Tell mono to set this domain as the default one.
-        // Second parameter forces this domain to be used
-        mono_domain_set(appDomain, true);
     }
 
     void Engine::cleanup()
     {
-        mono_jit_cleanup(rootDomain);
-        rootDomain = nullptr;
-        appDomain = nullptr;
         SlagLib::cleanup();
     }
 } // crucible
