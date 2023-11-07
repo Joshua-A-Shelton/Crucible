4f2ed78fffec51307f68574ab51496ce583e2f11
jshelton <joashel@gmail.com>
2023-11-07 12:57:41 -0700

    Windows string handling for loading dot net runtime

--- src/crucible/Scripting/ScriptingEngine.cpp
+++ src/crucible/Scripting/ScriptingEngine.cpp
@@ -9,7 +9,7 @@
 
 #define STR(s) L ## s
 #define CH(c) L ## c
-#define DIR_SEPARATOR L'\\'
+#define DIR_SEPARATOR "\\"
 
 #define string_compare wcscmp
 
@@ -42,7 +42,7 @@ namespace crucible
     void *load_library(const char_t *);
     void *get_export(void *, const char *);
 
-#ifdef WINDOWS
+#ifdef WIN32
     void *load_library(const char_t *path)
     {
         HMODULE h = ::LoadLibraryW(path);
@@ -54,6 +54,19 @@ namespace crucible
         void *f = ::GetProcAddress((HMODULE)h, name);
         assert(f != nullptr);
         return f;
+    }
+    std::wstring platformString(const std::string &multi)
+    {
+        std::wstring wide; wchar_t w; mbstate_t mb {};
+        size_t n = 0, len = multi.length () + 1;
+        while (auto res = mbrtowc (&w, multi.c_str () + n, len - n, &mb)) {
+            if (res == size_t (-1) || res == size_t (-2))
+                throw "invalid encoding";
+
+            n += res;
+            wide += w;
+        }
+        return wide;
     }
 #else
     void *load_library(const char_t *path)
@@ -68,6 +81,11 @@ namespace crucible
         assert(f != nullptr);
         return f;
     }
+
+    std::string& platformString(std::string& multi)
+    {
+        return multi;
+    }
 #endif
 
 
@@ -78,11 +96,11 @@ namespace crucible
         auto executableDirectory = boost::filesystem::current_path();
 
         loadHostFXR();
-        load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = get_dotnet_load_assembly((executableDirectory.string() + DIR_SEPARATOR+ "config"+DIR_SEPARATOR+"CSharpConfig.json").c_str());
+        load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = get_dotnet_load_assembly(platformString(executableDirectory.string() + DIR_SEPARATOR+ "config"+DIR_SEPARATOR+"CSharpConfig.json").c_str());
 
         component_entry_point_fn hello = nullptr;
-        int rc = load_assembly_and_get_function_pointer(
-                (executableDirectory.string() + DIR_SEPARATOR + "Crucible.dll").c_str(),
+        load_assembly_and_get_function_pointer(
+                platformString(executableDirectory.string() + DIR_SEPARATOR + "Crucible.dll").c_str(),
                 STR("DotNetLib.Lib, DotNetLib"),
                 STR("Hello"),
                 nullptr,
