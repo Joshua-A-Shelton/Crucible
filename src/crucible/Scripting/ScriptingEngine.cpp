38918369d18335369fe8878641a596899a545aad
Josh <joashel@gmail.com>
2023-11-07 17:45:27 -0700

    c# accessible from linux

--- src/crucible/Scripting/ScriptingEngine.cpp
+++ src/crucible/Scripting/ScriptingEngine.cpp
@@ -82,7 +82,7 @@ namespace crucible
         return f;
     }
 
-    std::string& platformString(std::string& multi)
+    std::string platformString(const std::string& multi)
     {
         return multi;
     }
