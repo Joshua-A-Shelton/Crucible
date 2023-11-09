0908a9cae9ccb4bc8e345b137d42666f7155a703
Josh <joashel@gmail.com>
2023-11-08 23:00:43 -0700

    started runtime libarary

---
+++ dotnet/Crucible/Crucible/Vector3.cs
@@ -0,0 +1,13 @@
+using System.Runtime.InteropServices;
+
+namespace Crucible;
+
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Vector3
+{
+    public float x;
+    public float y;
+    public float z;
+    
+}
