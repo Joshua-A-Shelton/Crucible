55017d072ebd223bb4c8c13bb8f8317371a06a4f
jshelton <joashel@gmail.com>
2023-12-05 08:50:38 -0700

    exposed registering unmanaged functions

--- src/crucible/Scripting/ScriptingEngine.h
+++ src/crucible/Scripting/ScriptingEngine.h
@@ -18,12 +18,13 @@ namespace crucible
 
         static ManagedType getManagedType(const std::string& typeName);
         static void* getManagedFunction(ManagedType& type,const std::string& fullyQualifiedFunctionName);
+        static void registerUnmanagedFunction(const std::string& assemblyQualifiedClassName, const std::string& managedDelegateName, void** functionPointer);
         static void loadManagedDll(const char* path);
     private:
         static bool loadHostFXR();
         static load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *config_path);
 
-        static void registerUnmanagedFunction(const std::string& assemblyQualifiedClassName, const std::string& managedDelegateName, void** functionPointer);
+
 
 
     };
