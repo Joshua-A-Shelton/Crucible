67c13d535b1bd433af2541419ee0c489a6872e8a
jshelton <joashel@gmail.com>
2025-05-09 16:42:12 -0600

    removed default no culling of triangles that was used for debugging, and forgot to remove before committing

--- src/crucible/core/ShaderManager.cpp
+++ src/crucible/core/ShaderManager.cpp
@@ -177,7 +177,6 @@ namespace crucible
                 getDescriptorGroups(bytePointer,last,descriptorGroups);
 
                 slag::ShaderProperties properties{};
-                properties.rasterizationState.culling = slag::RasterizationState::CullOptions::NONE;
                 VertexInfo vertexInfo(slag::VertexDescription(0),std::vector<Mesh::VertexAttribute>({}));
                 slag::FrameBufferDescription frameBufferDescription;
                 std::vector<slag::ShaderModule> shaderModules;
